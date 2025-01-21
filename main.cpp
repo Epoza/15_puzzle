a5fe1f4dfd3c3cdf74696cd09e369cd9dc4386c7
Epoza <86025026+Epoza@users.noreply.github.com>
2025-01-20 23:00:28 -0500

    add board class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <vector>
+#include <array>
+
+// clear the previous game or other console messages
+constexpr int g_consoleLines{25};
 
 class Tile
 {
@@ -24,23 +29,46 @@ std::ostream &operator<<(std::ostream &out, Tile tile)
   if (tile.getNum() > 9) // two digits
     out << " " << tile.getNum() << " ";
   else if (tile.getNum() > 0) // single digit
-    out << "  " << tile.getNum() << "  ";
+    out << "  " << tile.getNum() << " ";
   else if (tile.getNum() == 0) // empty tile
     out << "   ";
   return out;
 }
+
+// Your code goes here
+class Board
+{
+private:
+  std::vector<std::vector<Tile>> m_gameboard{{Tile{1}, Tile{2}, Tile{3}, Tile{4}}, {Tile{5}, Tile{6}, Tile{7}, Tile{8}}, {Tile{9}, Tile{10}, Tile{11}, Tile{12}}, {Tile{13}, Tile{14}, Tile{15}, Tile{0}}};
+
+public:
+  Board() = default;
+
+  // output the board
+  friend std::ostream &operator<<(std::ostream &out, const Board &board)
+  {
+    for (int i{0}; i <= g_consoleLines; ++i)
+    {
+      std::cout << '\n';
+    }
+    // iterate through each row
+    for (const auto &row : board.m_gameboard)
+    {
+      // iterate through each tile in the row
+      for (const auto &tile : row)
+      {
+        out << tile;
+      }
+      out << '\n';
+    }
+    return out;
+  };
+};
+
 int main()
 {
-  Tile tile1{10};
-  Tile tile2{8};
-  Tile tile3{0}; // the missing tile
-  Tile tile4{1};
-
-  std::cout << "0123456789ABCDEF\n"; // to make it easy to see how many spaces are in the next line
-  std::cout << tile1 << tile2 << tile3 << tile4 << '\n';
-
-  std::cout << std::boolalpha << tile1.isEmpty() << ' ' << tile3.isEmpty() << '\n';
-  std::cout << "Tile 2 has number: " << tile2.getNum() << "\nTile 4 has number: " << tile4.getNum() << '\n';
+  Board board{};
+  std::cout << board;
 
   return 0;
 }
