8327b6778d26c893cd5df5064d952ff63087f6f4
Epoza <86025026+Epoza@users.noreply.github.com>
2025-01-21 22:05:53 -0500

    add user input & direction class

--- main.cpp
+++ main.cpp
@@ -1,10 +1,123 @@
 #include <iostream>
 #include <vector>
-#include <array>
+#include <cassert>
+#include <limits>
+#include "Random.h"
 
 // clear the previous game or other console messages
 constexpr int g_consoleLines{25};
 
+// convert a user inputted char to a specific direction
+class Direction
+{
+public:
+  enum Type
+  {
+    up,
+    down,
+    left,
+    right,
+    maxDirections
+  };
+
+  Direction(Type type) : m_type{type}
+  {
+  }
+
+  Type getType() const
+  {
+    return m_type;
+  }
+
+  Direction operator-() const
+  {
+    switch (m_type)
+    {
+    case up:
+      return Direction{down};
+    case down:
+      return Direction{up};
+    case left:
+      return Direction{right};
+    case right:
+      return Direction{left};
+    default:
+      break;
+    }
+
+    assert(0 && "Unsupported direction was passed!");
+    return Direction{up};
+  }
+
+  static Direction getRandomDirection()
+  {
+    return Direction{static_cast<Type>(Random::get(0, Direction::maxDirections - 1))};
+  }
+
+private:
+  Type m_type{};
+};
+
+std::ostream &operator<<(std::ostream &out, Direction d)
+{
+  switch (d.getType())
+  {
+  case Direction::up:
+    return (out << "up");
+  case Direction::down:
+    return (out << "down");
+  case Direction::left:
+    return (out << "left");
+  case Direction::right:
+    return (out << "right");
+  default:
+    break;
+  }
+
+  assert(0 && "Unsupported direction was passed!");
+  return (out << "unknown direction");
+}
+
+// get and validate user input
+namespace UserInput
+{
+  bool isValid(char input)
+  {
+    return input == 'w' || input == 'a' || input == 's' || input == 'd' || input == 'q';
+  }
+
+  char getUserInput()
+  {
+    char input{};
+    while (!isValid(input))
+    {
+      std::cout << "Please enter a command: ";
+      std::cin >> input;
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    return input;
+  }
+
+  Direction convertToDirection(char input)
+  {
+    switch (input)
+    {
+    case 'w':
+      return Direction{Direction::up};
+    case 'a':
+      return Direction{Direction::left};
+    case 's':
+      return Direction{Direction::down};
+    case 'd':
+      return Direction{Direction::right};
+    }
+
+    assert(0 && "Unsupported direction was passed!");
+    return Direction{Direction::up};
+  }
+}
+
+// class for a single game tile
 class Tile
 {
 private:
@@ -35,7 +148,7 @@ std::ostream &operator<<(std::ostream &out, Tile tile)
   return out;
 }
 
-// Your code goes here
+// class for the board
 class Board
 {
 private:
@@ -69,6 +182,30 @@ int main()
 {
   Board board{};
   std::cout << board;
+
+  std::cout << "Generating random direction... " << Direction::getRandomDirection() << '\n';
+  std::cout << "Generating random direction... " << Direction::getRandomDirection() << '\n';
+  std::cout << "Generating random direction... " << Direction::getRandomDirection() << '\n';
+  std::cout << "Generating random direction... " << Direction::getRandomDirection() << "\n\n";
+
+  std::cout << "Enter a command: ";
+
+  while (true)
+  {
+    char ch{UserInput::getUserInput()};
+
+    // Handle non-direction commands
+    if (ch == 'q')
+    {
+      std::cout << "\n\nBye!\n\n";
+      return 0;
+    }
+
+    // Handle direction commands
+    Direction dir{UserInput::convertToDirection(ch)};
+
+    std::cout << "You entered direction: " << dir << '\n';
+  }
 
   return 0;
 }
